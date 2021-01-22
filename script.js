94bafc18d40a6448934ae30af3ec5ec34ff6d698
saifulislamsojib <saifulislam.sojib56@gmail.com>
2021-01-22 21:57:01 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const currentOperand = document.querySelector('.current-operand');
+const numbers = document.getElementsByClassName("number");
+const point = document.querySelector('.point');
+const allClear = document.querySelector('.all-clear');
+const backDelete = document.querySelector('.delete');
+const numberClick = (number) => {
+    currentOperand.innerText += number.innerText;
+}
+[...numbers].map(number => {
+    number.addEventListener('click', () => {
+        numberClick(number);
+    })
+})
+point.addEventListener('click', () => {
+    if (currentOperand.innerText.indexOf('.') == -1) {
+        numberClick(point);
+    }
+})
+allClear.addEventListener('click', () => {
+    currentOperand.innerText = '';
+})
+backDelete.addEventListener('click', () => {
+    const array = currentOperand.innerText.split('');
+    array.pop();
+    const str = array.join('');
+    currentOperand.innerText = str;
+})
