b539de618f0d52a35a0b9fdf6828d1b61c558e25
saifulislamsojib <saifulislam.sojib56@gmail.com>
2021-02-01 20:18:26 +0600

    finished

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 const currentOperand = document.querySelector('.current-operand');
+const previousOperand = document.querySelector('.previous-operand');
 const numbers = document.getElementsByClassName("number");
 const point = document.querySelector('.point');
 const allClear = document.querySelector('.all-clear');
-const backDelete = document.querySelector('.delete');
+const backDelete = document.querySelector('.delete'); 
+const operations = document.getElementsByClassName("operation");
+const equalOperation = document.querySelector('.equal-operation');
 const numberClick = (number) => {
     currentOperand.innerText += number.innerText;
 }
 [...numbers].map(number => {
     number.addEventListener('click', () => {
-        numberClick(number);
+        if (number.innerText == "0") {
+            if (currentOperand.innerText != "0") {
+                numberClick(number);
+            }
+        }
+        else {
+            numberClick(number);
+        }
     })
+});
+[...operations].map(operation => {
+    operation.addEventListener('click', () => {
+        if (currentOperand.innerText) {
+            previousOperand.innerText = currentOperand.innerText;
+            previousOperand.innerText += operation.innerText;
+            currentOperand.innerText = "";
+        }
+    })
+})
+equalOperation.addEventListener('click', () => {
+    const operand = previousOperand.innerText + currentOperand.innerText;
+    currentOperand.innerText = eval(operand);
+    previousOperand.innerText = '';
 })
 point.addEventListener('click', () => {
     if (currentOperand.innerText.indexOf('.') == -1) {
@@ -18,6 +42,7 @@ point.addEventListener('click', () => {
 })
 allClear.addEventListener('click', () => {
     currentOperand.innerText = '';
+    previousOperand.innerText = '';
 })
 backDelete.addEventListener('click', () => {
     currentOperand.innerText = currentOperand.innerText.slice(0, -1);
