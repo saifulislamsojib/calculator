cc66ee2f9dbfa8cdeb0a64cc771901b70fa88afe
saifulislamsojib <saifulislam.sojib56@gmail.com>
2021-02-02 01:41:26 +0600

    finish

--- script.js
+++ script.js
@@ -17,23 +17,56 @@ const numberClick = (number) => {
             }
         }
         else {
-            numberClick(number);
+            if (currentOperand.innerText === "0"){
+                currentOperand.innerText = "";
+            }
+            if (currentOperand.innerText.length === 14){
+                currentOperand.setAttribute("id", "size");
+            }
+            if (currentOperand.innerText.length !== 19){
+                numberClick(number);
+            }
         }
     })
 });
 [...operations].map(operation => {
     operation.addEventListener('click', () => {
+        currentOperand.innerText = withOutEval() || currentOperand.innerText;
         if (currentOperand.innerText) {
             previousOperand.innerText = currentOperand.innerText;
             previousOperand.innerText += operation.innerText;
             currentOperand.innerText = "";
         }
+        else{
+            if (previousOperand.innerText) {
+                previousOperand.innerText = previousOperand.innerText.slice(0, -1);
+                previousOperand.innerText += operation.innerText;
+            }
+        }
     })
 })
+const withOutEval = () => {
+    const previous = parseFloat(previousOperand.innerText.slice(0, -1));
+    const current = parseFloat(currentOperand.innerText);
+    const operator = previousOperand.innerText.split(previous)[1];
+    if (operator === "+") {
+        return previous + current;
+    }
+    else if (operator === "-") {
+        return previous - current;
+    }
+    else if (operator === "*") {
+        return previous * current;
+    }
+    else if (operator === "÷") {
+        return previous / current;
+    }
+}
 equalOperation.addEventListener('click', () => {
-    const operand = previousOperand.innerText + currentOperand.innerText;
-    currentOperand.innerText = eval(operand);
-    previousOperand.innerText = '';
+    currentOperand.innerText = withOutEval() || currentOperand.innerText;
+    if (currentOperand.innerText) {
+        previousOperand.innerText = '';
+    }
 })
 point.addEventListener('click', () => {
     if (currentOperand.innerText.indexOf('.') == -1) {
@@ -43,7 +76,11 @@ point.addEventListener('click', () => {
 allClear.addEventListener('click', () => {
     currentOperand.innerText = '';
     previousOperand.innerText = '';
+    currentOperand.removeAttribute("id", '');
 })
 backDelete.addEventListener('click', () => {
     currentOperand.innerText = currentOperand.innerText.slice(0, -1);
+    if (currentOperand.innerText.length === 14){
+        currentOperand.removeAttribute("id", "");
+    }
 })
